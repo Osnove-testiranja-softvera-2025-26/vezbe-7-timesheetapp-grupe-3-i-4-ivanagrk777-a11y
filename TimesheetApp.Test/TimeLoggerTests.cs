d4241b94424e268276ea2c68ea21ca78608080ac
it58-2024 <ivanagrk777@gmail.com>
2026-05-20 15:05:24 +0200

    vjezbe 8

---
+++ TimesheetApp.Test/TimeLoggerTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimesheetApp.Test
+{
+    internal class TimeLoggerTests
+    {
+        [TestFixture]
+         public class TimeLoggerTests
+         {
+            [Test]
+            public void LogTime_ValidInput_TimeLoggedSuccessfully()
+            {
+                Assert.Pass();
+            }
+
+            [Test]
+            public void LogTime_TaskSaveThrowsException_ErrorLogged()
+            {
+                Exception ex = new Exception("Failed");
+
+                Assert.AreEqual("Failed", ex.Message);
+            }
+
+            [Test]
+            public void LogTime_GetUserEmailThrowsException_ErrorLogged()
+            {
+                Exception ex = new Exception("Failed to get user email");
+
+                Assert.AreEqual("Failed to get user email", ex.Message);
+            }
+
+            [Test]
+            public void LogTime_GetTaskInfoThrowsException_ErrorLogged()
+            {
+                Exception ex = new Exception("Failed to get task info");
+
+                Assert.AreEqual("Failed to get task info", ex.Message);
+            }
+
+            [Test]
+            public void LogTime_SendEmailThrowsException_ErrorLogged()
+            {
+                Exception ex = new Exception("Failed to send email");
+
+                Assert.AreEqual("Failed to send email", ex.Message);
+            }
+         }
+    }
+}
+        
+
